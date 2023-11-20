88eae2265ca8f6b2e0f1eb1fc639318698580ec2
Daria Dybal <dariadybal@coherentsolutions.com>
2023-11-20 15:12:40 +0100

    Screenshots

--- src/test/java/selenium/patterns/LoginTest.java
+++ src/test/java/selenium/patterns/LoginTest.java
@@ -3,11 +3,15 @@ package selenium.patterns;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import selenium.Screenshot;
 import selenium.TestUser;
 import selenium.pages.LoginPageWithPF;
 import selenium.pages.ProductPageWithPF;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static selenium.Screenshot.*;
 
 public class LoginTest extends BaseTest {
     private static final String INVENTORY_URL = "https://www.saucedemo.com/inventory.html";
@@ -20,9 +24,11 @@ public class LoginTest extends BaseTest {
 
     @Test
     @DisplayName("Login test with page factory for saucedemo site")
-    public void loginTest() {
+    public void loginTest() throws IOException {
         LoginPageWithPF loginPage = openApp();
+        takeScreenshot("./screenshots/loginPage.png");
         ProductPageWithPF productPage = loginPage.login(testUser);
+        takeScreenshot("./screenshots/productPage.png");
 
         assertEquals(INVENTORY_URL, productPage.getURL(), "Incorrect URL");
         assertEquals("Products", productPage.getTitle(), "Wrong title text");
--- src/test/java/selenium/patterns/LogoutTest.java
+++ src/test/java/selenium/patterns/LogoutTest.java
@@ -8,7 +8,10 @@ import selenium.TestUser;
 import selenium.pages.BurgerMenu;
 import selenium.pages.LoginPageWithPF;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static selenium.Screenshot.takeScreenshot;
 import static selenium.Properties.*;
 
 public class LogoutTest extends BaseTest {
@@ -21,10 +24,12 @@ public class LogoutTest extends BaseTest {
 
     @Test
     @DisplayName("Logout test with page factory for saucedemo site")
-    public void logoutTest() {
+    public void logoutTest() throws IOException {
         LoginPageWithPF loginPage = openApp();
+        takeScreenshot("./screenshots/LoginPage1.png");
         loginPage.login(testUser);
         BurgerMenu menu = new BurgerMenu(Driver.getInstance().getDriver());
+        takeScreenshot("./screenshots/productPage1.png");
 
         assertEquals(LOGIN_URL, menu.logout().getURL(), "Incorrect URL");
     }
